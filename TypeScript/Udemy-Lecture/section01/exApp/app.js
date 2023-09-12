d2dca52e2f55e35cd23b5cff390b07808322cd23
SsangSoo <kss10080@naver.com>
2023-09-12 23:15:12 +0900

    until tuplegit status

--- TypeScript/Udemy-Lecture/section01/exApp/app.js
+++ TypeScript/Udemy-Lecture/section01/exApp/app.js
@@ -1,17 +1,36 @@
-function add(n1, n2, showResult, phrase) {
-    // if(typeof n1 !== 'number' || typeofn2 !== 'number') {
-    //     throw new Error("Incorrect input")
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
+// const person = {
+// name : 'Maximilan',
+// age : 30,
+// hobbies: ['Sports', 'Cooking'],
+// role: [2, 'author'] // 여기선 사용할 수 있는 타입이 무엇인지만 지정
+// 첫 번째 요소는 숫자, 두 번째 요소는 문자열 인것을 사용자만 알고, 타스는 알지 못함. 
+// 이런 경우에 튜플이 적합하다.
+// 어떤 role이어야 하는지를 role의 타입을 명시적으로 설정함으로써 타입스크립트에게 인식시켜야 함.
+// };
+// person.role.push('admin');  // 작동이 가능 // 작동이 되면 안 됨.
+// person.role[1] = 10;
+var person = {
+    name: 'Maximilan',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+};
+// 타입스크립트로 개발 도중 이렇게 작성하면 에러가 발생한다.
+// 이 코드에는 딱 두 개의 요소만 있는 특수 배열을 입력해야한다.
+person.role.push('admin');
+// person.role[1] = 10; 
+var favoriteActivities; // 배열
+// favoriteActivities = 'Sports';     // 문자열들의 배열이 아닌 하나의 값이기 때문에, 에러 발생
+favoriteActivities = ['Sports']; // 괄호로 감싸면 됨.
+// 숫자를 추가하면, 컴파일 오류 발생, 숫자도 함께 추가되려면, any[] 타입으로 선언해야함.
+favoriteActivities = ['Sports']; // 괄호로 감싸면 됨. 
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    // 문자열의 어떤 메서드도 사용가능
+    // 왜냐하면 타입스크립트가 hobbies가 문자열의 배열 타입이라고 이해하고 있기 때문
+    console.log(hobby.toUpperCase());
+    // 오류 발생
+    // map은 배열에서는 가능하지만, 문자열에선 사용 못함.   
+    // console.log(hobby.map());  
 }
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
