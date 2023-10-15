dabffa19e0694e84e7bf3c7d76c32f295d47762e
SsangSoo <kss10080@naver.com>
2023-10-15 22:24:49 +0900

    HTML Elements Control!

--- JS_Inflearn_By_GYM_Coding/lectures/2_dom-control/1_selector.js
+++ JS_Inflearn_By_GYM_Coding/lectures/2_dom-control/1_selector.js
@@ -15,6 +15,8 @@ console.log(liList);
 
 // 위의 방법은 Get을 통한 메서드로 HTML 요소 접근
 
+// 아래 방법은 요소 조작 혹은 제어
+
 let h2 = document.querySelector('#title');
 console.log('h2: ', h2);
 
@@ -26,3 +28,28 @@ console.log('itemAll: ', itemAll);
 console.log('itemAll[0]', itemAll[0]);
 console.log('itemAll[1]', itemAll[1]);
 console.log('itemAll[2]', itemAll[2]);
+
+// h2.textContent = '운동!!!';
+// h2.textContent = '<span>운동!!!</span>'; // 스팬태그 그대로 표현됨
+// console.log(h2.textContent);
+
+// h2.innerHTML = '<span>운동!!!</span>';
+
+// innerHTML 사용시 컨텐츠 안의 모든 게 지워지면서 값을 넣기 때문에 주의!
+// document.querySelector('body').innerHTML = '<strong>운동</strong>';
+
+// 태그를 가져와서 HTML 요소를 조작 및 제어
+let input = document.querySelector('input');
+input.setAttribute('placeholder', '헬스 운동을 입력해주세요!');
+input.setAttribute('required', '');
+input.removeAttribute('placeholder');
+input.removeAttribute('required');
+
+// 클래스가 속한 태그를 가져와서 제어
+let helloItem = document.querySelector('.hello');
+console.log('helloItem', helloItem);
+// helloItem.style.color = 'white';
+// helloItem.style.backgroundColor = 'black';
+
+helloItem.classList.add('dark', 'one');
+helloItem.classList.remove('dark');
