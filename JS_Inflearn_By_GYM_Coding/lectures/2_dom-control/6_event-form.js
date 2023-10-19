3d878410b305f9f74b9bab20be56955839465013
SsangSoo <kss10080@naver.com>
2023-10-20 00:11:43 +0900

    OK

--- JS_Inflearn_By_GYM_Coding/lectures/2_dom-control/6_event-form.js
+++ JS_Inflearn_By_GYM_Coding/lectures/2_dom-control/6_event-form.js
@@ -20,21 +20,27 @@ let inputBox = document.querySelector('#input-box');
 //     console.log('event.keyCode : ', event.keyCode); // 아스키코드로 확인 가능함.
 // })
 
-
-// 포커스 focus, blur,
--
-inputBox.addEventListener('focus', function (event) {
-    alert('focus');
-})
+// focus
+// inputBox.addEventListener('focus', function (event) {
+//       alert('focus');
+// });
 
 // blur
-// inputBox.addEventListener('focus', function (event) {
-//     console.log('event.key : ', event.key);         // 어떤 키보드로 쳤는지 확인 가능
-//     console.log('event.keyCode : ', event.keyCode); // 아스키코드로 확인 가능함.
+// inputBox.addEventListener('blur', function (event) {
+//     alert('blur');         // 어떤 키보드로 쳤는지 확인 가능
 // })
-//
-// inputBox.addEventListener('focus', function (event) {
-//     console.log('event.key : ', event.key);         // 어떤 키보드로 쳤는지 확인 가능
-//     console.log('event.keyCode : ', event.keyCode); // 아스키코드로 확인 가능함.
-// })
+
+// change - 체크박스, 라디오박스, 셀렉트 박스에서 자주 쓰임.
+// inputBox.addEventListener('change', function (event) {
+//     console.log('change');
+// });
+
+
+let img = document.querySelector('img');
+
+// error를 이용함
+img.addEventListener('error', function (eve) {
+    console.log('error');
+    event.target.src = "../../img/apple.jpg";
+});
 
