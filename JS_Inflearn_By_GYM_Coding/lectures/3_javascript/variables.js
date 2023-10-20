7cc54011f7860943c2d132709c7c3906438d391f
SsangSoo <kss10080@naver.com>
2023-10-20 22:02:27 +0900

    ok

---
+++ JS_Inflearn_By_GYM_Coding/lectures/3_javascript/variables.js
@@ -0,0 +1,19 @@
+// 변수 선언
+let teacher = '짐코딩';
+console.log(teacher);
+
+// 변수에 값 재하다
+teacher = '홍길동';
+console.log(teacher);
+
+// 이미 선언한 변수는 선언 못 함.
+// let teacher = '짐길동';
+
+// 상수 선언
+const username = '짐코딩';
+console.log(username);
+
+// 상수엔 값을 재할당 못함.
+// username = 'change';
+
+// 6분 10초~
