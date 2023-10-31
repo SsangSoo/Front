10b12e7ffd2c1c236ee8b5f4910f1c5cff8b0cad
SsangSoo <kss10080@naver.com>
2023-11-01 00:01:17 +0900

    Operator ING~~....

--- JS_Inflearn_By_GYM_Coding/lectures/3_javascript/operator.js
+++ JS_Inflearn_By_GYM_Coding/lectures/3_javascript/operator.js
@@ -1,9 +1,9 @@
-// 문자열 병합
-console.log('Hello ' + 'world!');   // Hello world!
-console.log('3' + 3);               // 33
-console.log(typeof ('3' + 3));      // string
-console.log('3' + 5 + 8);           // 358 // 덧셈은 왼쪽부터 진행됨
-console.log(3 + 5 + '8');           // 88 // 덧셈은 왼쪽부터 진행됨
+// // 문자열 병합
+// console.log('Hello ' + 'world!');   // Hello world!
+// console.log('3' + 3);               // 33
+// console.log(typeof ('3' + 3));      // string
+// console.log('3' + 5 + 8);           // 358 // 덧셈은 왼쪽부터 진행됨
+// console.log(3 + 5 + '8');           // 88 // 덧셈은 왼쪽부터 진행됨
 
 // 산술 연산자
 // let x = 10;
@@ -81,15 +81,69 @@ console.log(3 + 5 + '8');           // 88 // 덧셈은 왼쪽부터 진행됨
 
 
 // 할당 연산자
-let x = 10; // 10
-let y = 20; // 20
-// x = x + y;  // 30 >>>>> 할당 연산자
-x += y; // 30
+// let x = 10; // 10
+// let y = 20; // 20
+// // x = x + y;  // 30 >>>>> 할당 연산자
+// x += y; // 30
+//
+// // 할당연산자를 이용해서 1~10까지 더해보기
+// let sum = 0;
+// for (let i=1; i<=10; i++) {
+//     sum += i;
+//     console.log(`${i}번째 sum = ${sum}`); // 백틱(`) 쓰기
+// }
 
-// 할당연산자를 이용해서 1~10까지 더해보기
-let sum = 0;
-for (let i=1; i<=10; i++) {
-    sum += i;
-    console.log(`${i}번째 sum = ${sum}`); // 백틱(`) 쓰기
-}
 
+// and
+let x = true;
+let y = true;
+console.log(x && y); // 모두 true이므로 true
+
+x = true;
+y = false;
+console.log(x && y); // 하나라도 false 이므로 false
+
+x = false;
+y = true;
+console.log(x && y); // 하나라도 false 이므로 false
+
+x = false;
+y = false;
+console.log(x && y); // 모두 false 이므로 false
+
+// or
+x = true;
+y = true;
+console.log(x && y); // 모두 true이므로 true
+
+x = true;
+y = false;
+console.log(x || y); // 하나라도 true 이므로 true
+
+x = false;
+y = true;
+console.log(x || y); // 하나라도 true 이므로 true
+
+x = false;
+y = false;
+console.log(x || y); // 모두 false 이므로 false
+
+console.log('===============');
+
+// not
+x = true;
+console.log(!x); // true의 not이므로 false
+
+x = false;
+console.log(!x); // false의 not이므로 true
+
+let btn = document.querySelector('#btn'); // 아이디 실별자 '#'
+let emailEl = document.querySelector('[name=email]');
+let passwordEl = document.querySelector('[name=password]');
+btn.addEventListener('click', function () {
+    let email = emailEl.value;
+    let password = passwordEl.value;
+    console.log('email : ', email);
+    console.log('password : ', password);
+} )
+// 7분~
