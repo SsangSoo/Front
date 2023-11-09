6a354d44c8971cd43da60009a79a8f4895f4a597
SsangSoo <kss10080@naver.com>
2023-11-09 20:42:34 +0900

    npm learning~

---
+++ JS_Inflearn_By_GYM_Coding/front_add_wing/learn-npm/index.js
@@ -0,0 +1,4 @@
+const days = require('dayjs');
+// console.log(days('2019-01-25').format('[YYYYescape]  YYYY- MM-DDTHH:mm:ssZ[Z]'));
+console.log(days('2023-11-09').format('YYYY-MM-DD'));
+// YYYYescape  2019- 01-25T00:00:00+09:00Z
