fdd5683a93eeb288c3fe6cde08c3b329d60bc388
SsangSoo <kss10080@naver.com>
2023-11-12 02:31:26 +0900

    ok

--- JS_Inflearn_By_GYM_Coding/front_add_wing/js_module/commonjs/index.js
+++ JS_Inflearn_By_GYM_Coding/front_add_wing/js_module/commonjs/index.js
@@ -1,7 +1,14 @@
-const { perfectScore, sum, avg, subtract} = require('./math');
+const math = require('./math');
+
+// console.log('perfectScore : ', perfectScore);  // import한 math를 이용해서 접근
+// console.log('sum : ', sum(80, 10));
+// console.log('avg : ', avg(80, 90));
+// console.log('subtract : ', subtract(80, 90));
+
+console.log('perfectScore : ', math.perfectScore);  // import한 math를 이용해서 접근
+console.log('sum : ', math.sum(80, 10));
+console.log('avg : ', math.avg(80, 90));
+console.log('subtract : ', math.subtract(80, 90));
 
 
-console.log('perfectScore : ', perfectScore);  // import한 math를 이용해서 접근
-console.log('sum : ', sum(80, 10));
-console.log('avg : ', avg(80, 90));
-console.log('subtract : ', subtract(80, 90));
+// common JS는 노드환경에서 확인한다.
--- JS_Inflearn_By_GYM_Coding/front_add_wing/js_module/commonjs/math.js
+++ JS_Inflearn_By_GYM_Coding/front_add_wing/js_module/commonjs/math.js
@@ -1,16 +1,40 @@
-exports.perfectScore = 100; // exports로 내보내기 가능
+//  모듚 내보내기
+// exports.perfectScore = 100; // exports로 내보내기 가능
+//
+// exports.sum = (num1, num2) => {
+//     return num1 + num2;
+// };
+//
+//
+// exports.avg = (num1, num2) => {
+//     return (num1 + num2) / 2;
+// };
+//
+//
+// exports.subtract = (num1, num2) => {
+//     return num1 - num2;
+// };
+//
 
-exports.sum = (num1, num2) => {
+
+perfectScore = 100; // exports로 내보내기 가능
+
+sum = (num1, num2) => {
     return num1 + num2;
 };
 
-
-exports.avg = (num1, num2) => {
+avg = (num1, num2) => {
     return (num1 + num2) / 2;
 };
 
-
-exports.subtract = (num1, num2) => {
+subtract = (num1, num2) => {
     return num1 - num2;
 };
 
+module.exports = {
+    perfectScore,
+    sum,
+    avg,
+    subtract,
+
+}
